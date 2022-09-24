3af6422832af062ef992f91093e21947a06c0890
Benkip001 <kiptoo925@gmail.com>
2022-09-24 11:34:15 +0300

    front page done,adding js remaining

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//select the elements
+var toggle_btn = document.querySelector(".toggle-btn");
+var big_wrapper = document.querySelector(".big-wrapper");
+const main = document.querySelector("main");
+
+function toggleAnimation(){
+
+}
+
+toggle_btn.addEventListener("click", toggleAnimation);

ae41b1fe4355d5cc41537349808b61f7f9055c0a
Benkip001 <kiptoo925@gmail.com>
2022-09-24 12:32:00 +0300

    javascript done

--- scripts.js
+++ scripts.js
@@ -1,10 +1,43 @@
 //select the elements
-var toggle_btn = document.querySelector(".toggle-btn");
-var big_wrapper = document.querySelector(".big-wrapper");
-const main = document.querySelector("main");
+var toggle_btn;
+var big_wrapper;
 
-function toggleAnimation(){
-
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
 }
 
-toggle_btn.addEventListener("click", toggleAnimation);
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation(){
+    //clone the wrapper
+    dark = !dark;
+    let clone = big_wrapper.cloneNode(true);
+    if(dark){
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    }else{
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy")
+    main.appendChild(clone);
+
+    clone.addEventListener("animationend", ()=>{
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+        //Reset variables
+        declare();
+        events()
+    });
+}
+
+function events(){
+    toggle_btn.addEventListener("click", toggleAnimation);
+}
+
+events();
